21481bf8335894fd7f8525c1e95264a57977bc73
¨rodrigo¨ <rodrigoe.99@hotmail.com>
2022-02-07 00:54:22 +1345

    terminado

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const dataProducts=require('./products.json');
+const dataclientes=require('./clientes.json');
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use(express.json());
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.render('index',{
+    destacados:dataProducts,
+        clientess:dataclientes 
+     })
+});
+
+
+app.get('/index',(req,res)=>{
+    res.render('./index',{
+    destacados:dataProducts,
+    clientess:dataclientes 
+    })
+});
+
+
+app.get('/contactos',(req,res)=>{
+    res.render('./pages/contactos')
+})
+
+app.get('/iniciarsecion',(req,res)=>{
+    res.render('./pages/iniciarsecion')
+});
+
+app.get('/productos',(req,res)=>{
+        res.render('./pages/productos') 
+});
+    
+app.listen(3000,()=>{
+    console.log("escuchando el puerto 3000")
+});
